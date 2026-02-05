bb242c4d12fd1ea0fa955831823b750ca7c28514
Darius Watson <26DJW03@stu.cpsk12.org>
2026-02-05 11:44:00 -0600

    More work on the Spawner class todo: spawning logic, destroy() waiting on: checkCollision, tick system

---
+++ src/Entity/Spawner/Spawner.cpp
@@ -0,0 +1,28 @@
+#include "Spawner.h"
+
+Spawner::Spawner()
+{
+	health = 20;
+}
+
+Spawner::~Spawner()
+{
+
+}
+
+const void Spawner::spawn_enemy(int tick)
+{
+	if (tick % spawn_rate == 0) //every 300 frames
+	{
+		//spawning logic
+	}
+}
+
+void Spawner::update_health(int tick)
+{
+	//checkCollision(Entity);
+	if (health <= 0)
+	{
+		destroy();
+	}
+}

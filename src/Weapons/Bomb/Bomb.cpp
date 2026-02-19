f84c752d931b7192a30a899948a8ea318f2fe6ab
Darius Watson <26DJW03@stu.cpsk12.org>
2026-02-19 10:43:17 -0600

    fixed aspect ratio

--- src/Weapons/Bomb/Bomb.cpp
+++ src/Weapons/Bomb/Bomb.cpp
@@ -5,7 +5,7 @@ Bomb::Bomb(sf::Vector2f playerPos) : Weapons(playerPos)
 {
 	velocity = sf::Vector2f(0.f, 5.f);
 	damage = 5;
-	//sprite->setTextureRect(sf::IntRect{ sf::Vector2i{70,35},sf::Vector2i{8,16} });
+	sprite->setTextureRect(sf::IntRect{ sf::Vector2i{51,5},sf::Vector2i{6,6} });
 }
 
 

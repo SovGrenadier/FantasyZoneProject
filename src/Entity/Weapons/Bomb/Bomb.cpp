a35b6accdd07d815c05619ae95870a846b8751cd
Hadeel Adam <27HIA01@stu.cpsk12.org>
2026-02-23 10:37:38 -0600

    bullet direction update

--- src/Entity/Weapons/Bomb/Bomb.cpp
+++ src/Entity/Weapons/Bomb/Bomb.cpp
@@ -4,7 +4,7 @@
 Bomb::Bomb(sf::Vector2f playerPos) : Weapons(playerPos)	
 {
 	damage = 5;
-	sprite->setTextureRect(sf::IntRect{ sf::Vector2i{51,5},sf::Vector2i{6,6} });
+	sprite->setTextureRect(sf::IntRect{ sf::Vector2i{62,6},sf::Vector2i{6,6} });
 }
 
 
@@ -20,5 +20,5 @@ void Bomb::death()
 
 void Bomb::update(int input)
 {
-	sprite->move({0, 1.5});
+	sprite->move({ 0, 1.5 });
 }

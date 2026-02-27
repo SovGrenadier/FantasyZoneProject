4e6b9ce2f406c9ab28fe6c426b09d4493fbc42e7
azur <27am01@stu.cpsk12.org>
2026-02-27 12:00:39 -0600

    framework for bottaco movement

--- src/Entity/Enemy/Bottaco/Bottaco.cpp
+++ src/Entity/Enemy/Bottaco/Bottaco.cpp
@@ -7,7 +7,7 @@ Bottaco::Bottaco(bool isFaceRight) : Enemy()
 	ticks = 13;
 	pos = { 840.f, 30.f };
 
-	sf::IntRect zone({ 11, 415 }, { 40, 16 });
+	sf::IntRect zone({ 10, 55}, { 63, 20});
 	Animation* fly = new Animation(1, 3, zone);
 
 	curAction = FLY_RIGHT;
@@ -15,6 +15,13 @@ Bottaco::Bottaco(bool isFaceRight) : Enemy()
 
 	sprite->setTexture(*texture);
 	sprite->setPosition(pos);
+	
+	speed = .05f;
+	amplitudeX = 10.f;
+	amplitudeY = 5.f;
+	time = 0.f;
+	centerX = pos.x;
+	centerY = pos.y;
 }
 
 
@@ -25,26 +32,37 @@ Bottaco::~Bottaco()
 
 void Bottaco::spawn()
 {
-
-}
-
-
-void Bottaco::move()
-{
-	//move logic
-	//moves like a figure 8/infinity symbol
 	//Two different formations
 	/*
 	1st Formation:
-	one enemy enters from both side of ths screen, 
-	they come towards the middle following the 
+	one enemy enters from both side of ths screen,
+	they come towards the middle following the
 	figure 8 shape, then go off screen
 	2nd Formation:
 	a column of 4 enemy appears from one side of the screen
 	follow the same movement
 	*/
+}
 
+
+void Bottaco::move()
+{
+	//moves like a figure 8/infinity symbol
+	//parametric equations
+
+	float xMovement = static_cast<float>(sin(time*2));
+	float yMovement = static_cast<float>(sin(time));
+
+	if (faceRight)
+	{
+		pos.x = centerX + amplitudeX * xMovement;
+		pos.y = centerY + amplitudeY * yMovement;
+	}
+
+
+	time += 0.05f;
 	sprite->setPosition(pos);
+	
 }
 
 

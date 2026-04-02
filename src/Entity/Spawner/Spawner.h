82149a26bf86a567e5ed5f8d0b52d654c497d82c
azurr <27am01@stu.cpsk12.org>
2026-04-02 12:00:39 -0500

    spawner death animation work in progress

--- src/Entity/Spawner/Spawner.h
+++ src/Entity/Spawner/Spawner.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <unordered_map>
 #include "../Enemy/Enemy.h"
 
 class Spawner : public Entity
@@ -12,6 +13,13 @@ public:
 
 	*/
 
+	enum Actions
+	{
+		ACTIVEFLY, // if in the air
+		ACTIVEGROUND, // if on the wall/ground
+		DEATH
+	};
+
     void spawnEnemy(int);
 	void updateHealth(int); //runs every tick, checks for damage
 	void update(int input) override;
@@ -19,15 +27,15 @@ public:
 	sf::Vector2f position;
 
 private:
+	Actions curAction;
 	int spawn_rate = 5 * 60; //5 seconds between each spawn sequence, 60 fps
 	int spawnerDist;
 
-	sf::Sprite* deathSprite;
-	sf::Texture* deathTexture;
 
 	//Inheriting from enemy causes problems so we just steal it instead
 	unsigned int curDeathFrame; 
 	std::vector <sf::IntRect> deathFrames;
+	std::unordered_map<Actions, Animation*> animations;
 	unsigned int tickRate;
 	sf::Vector2f deathPos;
 };

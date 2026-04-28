e986d02d4f1d1506738dafb05d92d83d421b1b57
SwissAdmiral <budapestcashew@gmail.com>
2026-04-28 11:41:22 -0500

    fixed coin, shop

--- src/Entity/Shop/Coin.cpp
+++ src/Entity/Shop/Coin.cpp
@@ -3,13 +3,14 @@
 
 Coin::Coin(sf::Vector2f pos)
 {
+	texture = new sf::Texture();
+
 	if (!texture->loadFromFile("../res/Shop Transparent.png"))
 		std::cout << "\nCoin: Error Loading from File\n";
-
 	//sprite->setTexture(*texture); 
+	sprite = new sf::Sprite(*texture);
 	zone = sf::IntRect(sf::Vector2i{ 444, 186 }, sf::Vector2i{ 21,13 });
-
-	//sprite->setTextureRect(zone); 
+	sprite->setTextureRect(zone); 
 }
 
 Coin::~Coin()
--- src/Game/Game.h
+++ src/Game/Game.h
@@ -16,7 +16,7 @@
 #include "../Entity/Weapons/Bullet/Leaf.h"
 #include "../Entity/Enemy/Boss/Boss.h"
 #include "../Entity/UI/UI.h"
-#include"../../FantasyZoneProject/Coin.h"
+#include"../Entity/Shop/Coin.h"
 #pragma once
 
 class Game {

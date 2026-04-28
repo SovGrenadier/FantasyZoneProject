9a5c00b71f666485797ab0b3a79cc9f1d6dd00aa
SwissAdmiral <budapestcashew@gmail.com>
2026-04-28 11:21:17 -0500

    coin fixes

--- src/Entity/Shop/Coin.cpp
+++ src/Entity/Shop/Coin.cpp
@@ -3,8 +3,8 @@
 
 Coin::Coin(sf::Vector2f pos)
 {
-	if (!texture->loadFromFile("../../res/Shop Transparent.png"))
-		std::cout << "\nCoin: Error Loading from File\n"; 
+	if (!texture->loadFromFile("../res/Shop Transparent.png"))
+		std::cout << "\nCoin: Error Loading from File\n";
 
 	//sprite->setTexture(*texture); 
 	zone = sf::IntRect(sf::Vector2i{ 444, 186 }, sf::Vector2i{ 21,13 });

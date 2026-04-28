1fd979e77798f87f2806663e294befbb13374d26
PhilipChindris <pchindris03@gmail.com>
2026-04-28 11:22:37 -0500

    Ability to continue to play the game after a player death. The window doesn't automatically close. Instead, it sends you back to the start screen.

--- src/Game/Game.h
+++ src/Game/Game.h
@@ -28,6 +28,8 @@ private:
 	sf::RenderWindow window;
 	sf::Texture background1;
 	sf::Sprite* backgroundSprite1;
+	sf::Texture backgroundStart;
+	sf::Sprite* backgroundSpriteStart;
 	sf::View viewport;
 	sf::View viewportStart;
 
@@ -55,12 +57,14 @@ private:
 	static int getRandomInt(int min, int max);
 	void initialize(); 
 	int score;
+	int highScore;
 	bool invincible = false;
 	bool shopSpawned = false;
 	std::string UItext = "";
 	std::string UItext2 = "";
 	std::string UItext3 = "";
 	std::string scoreStr = "";	
+	std::string highScoreStr = "";
 	std::string tempStr;
 	void removeDead(); 
 	void removeEnemies();
